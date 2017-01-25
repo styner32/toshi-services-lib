78aec2f790c5e26053d91f8757abe2af6eb9d0e1
Tristan King <tristan@bakkenbaeck.no>
2017-01-25 14:07:09 +0100

    added support for query string auth

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ setup(
     url='http://github.com/tokenbrowser/token-services-lib',
     description='',
     long_description=open('README.md').read(),
+    setup_requires=['pytest-runner'],
     install_requires=[
         'asyncbb==0.0.1',
         'tokenbrowser==0.0.1'
     ],
     dependency_links=[
         'http://github.com/tristan/asyncbb/tarball/master#egg=asyncbb-0.0.1',
-        'http://github.com/tokenbrowser/tokenbrowser-python/tarball/master#egg=tokenbrowser-0.0.1'
+        #'http://github.com/tokenbrowser/tokenbrowser-python/tarball/master#egg=tokenbrowser-0.0.1'
+        'git+ssh://git@github.com/tokenbrowser/tokenbrowser-python.git#egg=tokenbrowser-0.0.1'
+    ],
+    tests_require=[
+        'pytest'
     ]
 )
